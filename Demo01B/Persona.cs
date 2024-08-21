399944521af2e49eb32a48dbd696d0310254793b
Alumno <Alumno@LAB1507-02>
2024-08-20 19:51:15 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Demo01B
+{
+    public class Persona
+    {
+        public Persona (string nombres, string apellidos)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+        }
+
+        public Persona()
+        {
+        }
+
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+
+        public string NombresCompletos { get; set; }
+
+        public string EsMayorEdad()
+        {
+            if (Edad >= 18)
+            {
+                return "La persona es mayor de edad";
+            }
+            else
+            {
+                return "La persona es menor de edad";
+            }
+
+        }
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+        }
+    }
+}
---
+++ Demo01B/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+            Persona persona = new Persona();
+            persona.Nombres = "Pedro";
+            persona.Apellidos = "Hernandez Carhuajulca";
+
+            Persona persona1 = new Persona("Luisa", "Herrera");
+
+            Persona persona2 = new Persona
+            {
+                Nombres = "Luisa",
+                Apellidos = "Herrera",
+                Edad = 5
+            };
+            persona.AsignarNombresCompletos();
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.Altura = 5;
+            triangulo.Base = 6;
+            triangulo.CalcularArea();
+
+
+            Console.WriteLine(triangulo.CalcularArea());
+            Console.WriteLine(persona.NombresCompletos);
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+
+        }
+    }
+}
